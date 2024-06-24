ed14ece5b18fce15871c2032f882e02625662d6e
Tihomir Zhelyazkov <tihomirtx88@gmail.com>
2024-06-24 18:36:22 +0300

    Creating db.js and implemeting code for conection between express and mongoose

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const databaseConfig = require(`./config/db`);
 const morgan = require('morgan');
 
 const tourRouter = require('./routes/tourRoutes');
@@ -6,27 +7,36 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-// 1. MIDDLEWARES
+start();
 
-// Middleware for reading file with data
-app.use(express.json());
+async function start() {
 
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+  await databaseConfig(app);
+
+  // 1. MIDDLEWARES
+
+  // Middleware for reading file with data
+  app.use(express.json());
+
+  if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+  }
+
+  //Taking static files
+  app.use(express.static(`${__dirname}/public`));
+
+  // Custom time middleware
+  app.use((req, res, next) => {
+    req.requestTime = new Date().toISOString();
+    next();
+  });
+
+  //3 .Mount ROUTES
+
+  app.use('/api/v1/tours', tourRouter);
+  app.use('/api/v1/users', userRouter);
+
 }
 
-//Taking static files
-app.use(express.static(`${__dirname}/public`));
-
-// Custom time middleware
-app.use((req, res, next) => {
-  req.requestTime = new Date().toISOString();
-  next();
-});
-
-//3 .Mount ROUTES
-
-app.use('/api/v1/tours', tourRouter);
-app.use('/api/v1/users', userRouter);
-
 module.exports = app;
+
