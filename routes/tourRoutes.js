f6dafb6f4d9d95b0f61ae325bd76fe4df24482cf
Tihomir Zhelyazkov <tihomirtx88@gmail.com>
2024-06-30 15:56:28 +0300

    Implementing tour stats with agragation pipe by mongo and montly plan feature

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -5,6 +5,10 @@ const router = express.Router();
 
 router.route('/top-5-cheap').get(tourController.aliasTopTours, tourController.getallTours);
 
+router.route('/tour-stats').get(tourController.getTourStats);
+
+router.route('/montly-plan/:year').get(tourController.getMontlyPlan);
+
 router.route('/').get(tourController.getallTours).post(tourController.createTour);
 
 router
