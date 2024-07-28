83ccf30e084b7ddad70f8109a0dd160d5851ab7a
Tihomir Zhelyazkov <tihomirtx88@gmail.com>
2024-07-28 10:35:01 +0300

    Adding missing authentication and athorization

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,22 +7,37 @@ const router = express.Router();
 
 router.post('/signup', authController.signUp);
 router.post('/login', authController.login);
-
-router.patch('/updatePassword',authController.protect, authController.updatePassword);
-
-// Siwth params id with user id or some kind get data for current user
-router.get('/getMe', authController.protect, userController.getMe, userController.getUser);
-router.patch('/updateMe',authController.protect, userController.updateMe);
-router.delete('/deleteMe',authController.protect, userController.deleteMe);
-
 // Recive email
 router.post('/forgotPassword', authController.forgotPassword);
 
 //recive token
 router.patch('/resetPassword/:token', authController.resetPassword);
 
-router.route('/').get(userController.getAllUsers).post(userController.createUser);
+router.patch(
+  '/updatePassword',
+  authController.protect,
+  authController.updatePassword
+);
 
-router.route('/:id').get(userController.getUser).patch(userController.updateUser).delete(userController.deleteMe);
+//That mean i will use authController.protect middleware for the rest of code
+router.use(authController.protect);
+
+// Siwth params id with user id or some kind get data for current user
+router.get('/getMe', userController.getMe, userController.getUser);
+router.patch('/updateMe', userController.updateMe);
+router.delete('/deleteMe', userController.deleteMe);
+
+router.use(authController.restrictTo('admin'));
+
+router
+  .route('/')
+  .get(userController.getAllUsers)
+  .post(userController.createUser);
+
+router
+  .route('/:id')
+  .get(userController.getUser)
+  .patch(userController.updateUser)
+  .delete(userController.deleteMe);
 
 module.exports = router;
