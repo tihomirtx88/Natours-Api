4a5f213c1e47db3c7a5c8248358f562aa5f70e32
Tihomir Zhelyazkov <tihomirtx88@gmail.com>
2024-08-06 17:50:52 +0300

    Last chnages 06.08.2024

--- app.js
+++ app.js
@@ -29,6 +29,7 @@ const reviewRouter = require('./routes/reviewRoutes');
 
 const app = express();
 app.use(cors({
+  origin: 'http://localhost:3000', // Replace with your frontend domain
   credentials: true
 }));
 
