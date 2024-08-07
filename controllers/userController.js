61e0a42b3eee7a276ea366649e924e4490af5def
Tihomir Zhelyazkov <tihomirtx88@gmail.com>
2024-08-07 20:36:01 +0300

    Set multer librarry and use it in user controller in midleware UpdateMe

--- controllers/userController.js
+++ controllers/userController.js
@@ -32,6 +32,7 @@ exports.getAllUsers = catchAsync(async (req, res, next) => {
 });
 
 exports.updateMe = catchAsync(async (req, res, next) => {
+  
   //1. Create error if user POSTs password data
   if (req.body.password || req.body.passwordConfirm) {
     return next(
