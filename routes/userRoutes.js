1682f8d9b7a818e57e6b9f9fc22a46c6652f9152
Tihomir Zhelyazkov <tihomirtx88@gmail.com>
2024-08-07 19:51:01 +0300

    Create logout functionality to delete cookie in auth controller

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,6 +7,7 @@ const router = express.Router();
 
 router.post('/signup', authController.signUp);
 router.post('/login', authController.login);
+router.get('/logout', authController.logout);
 // Recive email
 router.post('/forgotPassword', authController.forgotPassword);
 
