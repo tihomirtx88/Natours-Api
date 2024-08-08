eca680aea62b493366669b571968a0e034783219
Tihomir Zhelyazkov <tihomirtx88@gmail.com>
2024-08-08 21:08:13 +0300

    Procees uploading images and atach on uploding tour data route

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -33,18 +33,42 @@ const upload = multer({
 });
 
 exports.uploadTourImages = upload.fields([
-  {name: 'imageCover', maxCount: 1},
-  {name: 'images', maxCount: 3}
+  { name: 'imageCover', maxCount: 1 },
+  { name: 'images', maxCount: 3 }
 ]);
 
 // upload.single('image');  req.file
 // upload.array('images', 5);  req.files
 
-exports.resizeTourimages = (req, res, next ) => {
-  if (!req.file) return next();
+exports.resizeTourimages = catchAsync(async (req, res, next) => {
+  if (!req.files.imageCover || !req.files.images) return next();
 
+  // 1. Cover image
+  req.body.imageCover = `tour-${req.params.id}-${Date.now()}-cover.jpeg`;
 
-};
+  await sharp(req.files.imageCover[0].buffer) //[0] because are imageCover is array
+    .resize(2000, 1333)
+    .toFormat('jpeg')
+    .jpeg({ quality: 90 })
+    .toFile(`public/img/tours/${req.body.imageCover}`);
+
+  // 2. Images
+  req.body.images = [];
+
+  await Promise.all(req.files.images.map(async (file, index) => {
+    const fileName = `tour-${req.params.id}-${Date.now()}-${index + 1}.jpeg`;
+
+    await sharp(file.buffer)
+      .resize(2000, 1333)
+      .toFormat('jpeg')
+      .jpeg({ quality: 90 })
+      .toFile(`public/img/tours/${fileName}`);
+
+    req.body.images.push(fileName);
+  }));
+
+  next();
+});
 
 // Prefilling query string
 exports.aliasTopTours = (req, res, next) => {
--- controllers/userController.js
+++ controllers/userController.js
@@ -42,20 +42,20 @@ exports.uploadUserImage = upload.single('photo');
 
 // Rezie image functionality
 
-exports.resizeUserPhoto = (req, res, next) => {
+exports.resizeUserPhoto = catchAsync(async (req, res, next) => {
   if (!req.file) return next();
 
   req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
 
   //Because using multer.memoryStorage() instead local storage
-  sharp(req.file.buffer)
+  await sharp(req.file.buffer)
     .resize(500, 500)
     .toFormat('jpeg')
     .jpeg({ quality: 90 })
     .toFile(`public/img/users/${req.file.filename}`);
 
   next();
-};
+});
 
 const filteredObj = (obj, ...allowedFields) => {
   const newObj = {};
